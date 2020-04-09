f82f88a16e0f33756312e785f3fe070837e485b4
Quentin Halsey <quentin.developer@outlook.com>
2020-04-08 20:38:11 -0700

    First commit, may update in the future to keep user data, and persistant

---
+++ src/context/GlobalState.js
@@ -0,0 +1,48 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: "Udemy Course", amount: -25 },
+    { id: 2, text: "Cup of Coffee", amount: -5 },
+    { id: 3, text: "Laptop Stickers", amount: -10 },
+    { id: 4, text: "Hosting", amount: -10 },
+    { id: 5, text: "Salary", amount: 100 },
+  ],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
